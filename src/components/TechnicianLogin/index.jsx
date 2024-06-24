3aa7972d3466c4faddbe90771245687644705f69
Aviezer <aviezerp@gmail.com>
2024-06-24 17:15:21 +0300

    Technician login

--- src/components/TechnicianLogin/index.jsx
+++ src/components/TechnicianLogin/index.jsx
@@ -9,19 +9,26 @@ export default function TechnicianLogin() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.Air_force_logo_wrapper}>
-                <img src="/images/Air_force_logo.png" alt="Air_force_logo" className={styles.Air_force_logo} />
+            <img src="/images/Air_force_logo.png" alt="Air_force_logo" className={styles.Air_force_logo} />
+            <h1 className={styles.loginText}>כניסה למערכת</h1>
+            <div className={styles.username}>
+                <label htmlFor='nameInput'>שם משתמש</label>
+                <Input id='nameInput' placeholder="שם משתמש" />
             </div>
-            <h1>כניסה למערכת</h1>
-            <Input placeholder={"שם משתמש"} />
-            <InputPassword />
-                <div className={styles.midLevel}>
-                    <label htmlFor=""> <input type="checkbox" /> זכור אותי</label>
-                    <Link style={{color: 'blue'}}>שכחת סיסמא?</Link>
-                </div>
-                <button className={styles.loginButton}>התחבר</button>
+            <div className={styles.password}>
+                <label htmlFor="pwd">סיסמא</label>
+                <InputPassword id='pwd' />
+            </div>
+            <div className={styles.midLevel}>
+                <label htmlFor=""> <input type="checkbox" /> זכור אותי</label>
+                <Link style={{ color: 'blue' }}>שכחת סיסמא?</Link>
+            </div>
+            <button className={styles.loginButton}>התחבר</button>
         </div>
     )
 }
+
+// <div className={styles.Air_force_logo_wrapper}>
+// </div>
 // <div className={styles.midLevelWrapper}>
 // </div>
