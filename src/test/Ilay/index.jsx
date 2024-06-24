7b1e5634e560c2564a716721bfda2f858fb941c0
Ilay <ilaygut123@gmail.com>
2024-06-24 13:24:11 +0300

    fixes for my test file (index & css)

---
+++ src/test/Ilay/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./style.module.css";
+
+// creator: Ilay
+// props: { title: string }
+export default function Ilay() {
+  return (
+    <div className={styles.Ilay}>
+      <h2>Ilay</h2>
+    </div>
+  );
+}
