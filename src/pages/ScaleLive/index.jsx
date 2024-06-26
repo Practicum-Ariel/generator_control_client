74e8bbd67a25a1236ed40a236bb46633f4d0a648
hodaya Turgeman <hodaya.tor@gmail.com>
2024-06-26 18:33:02 +0300

    finish live yaaaa

--- src/pages/ScaleLive/index.jsx
+++ src/pages/ScaleLive/index.jsx
@@ -7,7 +7,7 @@ import Loader from '../../components/Loader';
 
 const socketIO = io()
 
-export default function Hodaya({generatorId = "6678464e815884d6e23a4542"}) {
+export default function ScaleLive({generatorId = "6678464e815884d6e23a4542"}) {
     const [socket, setSocket] = useState(socketIO)
     const [genSensorsData, setgenSensorsData] = useState({})
     const {data=[], loading, error} = useApi(`/generator/${generatorId}/sensors`)
@@ -80,7 +80,7 @@ export default function Hodaya({generatorId = "6678464e815884d6e23a4542"}) {
                 } = n;
                 return ( 
                   <div key={index} className={styles.scaleChart}>
-                  <ScaleChart  key={index} size={genSensorsData[n.name]? genSensorsData[n.name] : 80  } unit={n.unitOfMeasure} name={n.name} rangeObject={{normalMin,normalMax,mildMin,mildMax,moderateMin,moderateMax,severe}}/>            
+                  <ScaleChart  key={index} size={genSensorsData[n.name]? genSensorsData[n.name] : 50  } unit={n.unitOfMeasure} name={n.name} rangeObject={{normalMin,normalMax,mildMin,mildMax,moderateMin,moderateMax,severe}}/>            
                   </div>
                 )}
               )}
@@ -123,52 +123,3 @@ export default function Hodaya({generatorId = "6678464e815884d6e23a4542"}) {
 
 
 
-
-
-
-
-// import ScaleChart from '../../components/ScaleChart';
-// import styles from './style.module.css';
-// import React from 'react';
-
-// export default function Hodaya() {
-
-//   const rangeObjectVib = 
-//   {
-//     normalMin:0,
-//     normlMax:10,
-//     mildMin:10,
-//     mildMax:15,
-//     moderateMin:15,
-//     moderateMax:20,
-//     severe:20
-//   }
-
-  
-//   const rangeObjectTemp = 
-  // {
-  //   normalMin:70,
-  //   normlMax:90,
-  //   mildMin:90,
-  //   mildMax:100,
-  //   moderateMin:100,
-  //   moderateMax:110,
-  //   severe:110
-  // }
-
-// return(
-//   <ScaleChart rangeObject={rangeObjectTemp }/>
-//   );
-// }
-
-
-
-
-// // background: linear-gradient(90deg,
-// //   rgba(241, 0, 0, 1) 0%,
-// //   rgba(233, 82, 17, 1) 20%,
-// //   rgba(23, 167, 9, 1) 58%, /*normal range*/
-// //   rgba(187, 232, 57, 1) 75%, /*mild anomaly*/
-// //   rgba(240, 167, 8, 1) 83%, /*moderat anomaly*/
-// //   rgba(191, 68, 3, 1) 91%, /*severe anomaly*/
-// //   rgba(240, 8, 8, 1) 100%); 
