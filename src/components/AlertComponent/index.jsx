488fae9b20f7a5f2ba4f06b65a26c55df27f63aa
ShaharEng <shaharbel0@gmail.com>
2024-06-26 17:39:45 +0300

    fix 1 shahar

--- src/components/AlertComponent/index.jsx
+++ src/components/AlertComponent/index.jsx
@@ -3,7 +3,7 @@ import styles from "./style.module.css";
 import { MdOutlineArrowLeft } from "react-icons/md";
 import { TiArrowSortedDown } from "react-icons/ti";
 
-export default function index({ status, title, context, handlleClick }) {
+export default function index({ status, title, description, context, treatments, handlleClick }) {
   const [isClose, setIsClose] = useState(true);
   return (
     // statuses[status] ?
@@ -13,12 +13,16 @@ export default function index({ status, title, context, handlleClick }) {
       <div className={styles.title}>
         {isClose ? <MdOutlineArrowLeft /> : <TiArrowSortedDown />}
         {title} 
-        <div className={styles.context}>{isClose ? (': ' + context) : ':'}</div>
+        <div className={styles.context}>: {description}</div>
       </div>
 
       {!isClose && (
         <>
           <div className="">{context}</div>
+          <div className={styles.treatments}>
+            <h5>דרכי טיפול:</h5>
+            {treatments.map(t => <li>{t}</li>)}
+          </div>
           <button onClick={() => handleClick} className={styles.but}>
             לפתיחת טיפול
           </button>
--- src/pages/SingleGenerator/index.jsx
+++ src/pages/SingleGenerator/index.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import styles from './styles.module.css'
 import useApi from '../../hooks/useApi';
 import Loader from '../../components/Loader';
 import AlertComponent from '../../components/AlertComponent';
+import BoxSensorType from '../../components/BoxSensorType';
 
 // creator: Shahar
 
 function SingleGenerator() {
-  let { id } = useParams();
   let nav = useNavigate()
+  let { id } = useParams();
+  const [insightBoxType, setInsightBoxType] = useState('all')
+  const [graphsBoxType, setGraphsBoxType] = useState('live')
 
-  // fake data
+  let statuses = ['success', 'warning', 'danger']
+
+  // #### fake data ####
   // let allGen = [{
   //   _id: '6678464e815884d6e23a4542',
   //   name: 'gen300',
@@ -30,6 +35,33 @@ function SingleGenerator() {
   //   location: 'ירושלים',
   //   status: 'בתיקון'
   // }]
+
+  let insights = [{
+    insight : 'The vibration readings show a pattern where the levels are within normal or mildly anomalous ranges but occasionally enter the moderate anomaly range. This indicates the need for regular monitoring and possible future maintenance.',
+    fault_name : 'Vibration Anomaly',
+    level_risk : 3,
+    fault_description : 'Vibration sensor readings indicating mild to moderate anomaly',
+    treatments : ['Monitor vibration levels regularly sagi audtae']
+  },
+  {
+    insight : 'Sound levels have been recorded near or slightly above the threshold for mild anomalies. Regular inspection and maintenance could help prevent further escalation.',
+    fault_name : 'Sound Anomaly',
+    level_risk : 2,
+    fault_description : 'Sound sensor readings indicating mild anomaly',
+    treatments : ['Monitor sound levels regularly', 'Inspect components near sound sensors for potential issues', 'Ensure all parts are well-lubricated and functioning properly', 'Schedule maintenance if sound levels consistently exceed the mild threshold']
+  },
+  {
+    insight : 'Temperature readings are within the normal operating range, indicating that the generator is maintaining proper thermal conditions.',
+    fault_name : 'Temperature Normal',
+    level_risk : 1,
+    fault_description : 'Temperature sensor readings within normal range',
+    treatments : ['Continue regular monitoring of temperature levels', 'Ensure coolant levels are maintained', 'Inspect cooling system components for wear and tear']
+  }]
+
+  const insightTypes = [{ text: "הכל", value: 'all' }, { text: "קל", value: 'succcess' }, { text: "בינוני", value: 'warning' }, { text: "קריטי", value: 'danger' }];
+  const graphsTypes = [{ text: "מידע בזמן אמת", value: 'live' }, { text: "היסטוריה", value: 'history' }];
+
+  // #### finish fake data ####
 
   const handleChange = (e) => {
     console.log(e.target.value);
@@ -59,17 +91,24 @@ function SingleGenerator() {
       </div>
       <div className={styles.insights}>
         <h3>תובנות Ai</h3>
+        <div className={styles.box_button}>
+          <BoxSensorType setSelected={setInsightBoxType} types={insightTypes} selected={insightBoxType} />
+        </div>
         <div className={styles.all_insights}>
-          <AlertComponent status={'danger'} title={'קריטי'} context={'הקירור בחריגות דלק, יש להחליף סנן ראשי במנוע. הקירור בחריגות דלק, יש להחליף סנן ראשי במנוע. הקירור בחריגות דלק, יש להחליף'} />
+          {/* <AlertComponent status={'danger'} title={'קריטי'} context={'הקירור בחריגות דלק, יש להחליף סנן ראשי במנוע. הקירור בחריגות דלק, יש להחליף סנן ראשי במנוע. הקירור בחריגות דלק, יש להחליף'} />
           <AlertComponent status={'succcess'} title={'הערה'} context={'האיזור חם'} />
           <AlertComponent status={'warning'} title={'לשים לב'} context={'הגנרטור מתחמם'} />
           <AlertComponent status={'danger'} title={'קריטי'} context={'הקירור בחריגות דלק, יש להחליף סנן ראשי במנוע'} />
           <AlertComponent status={'warning'} title={'לשים לב'} context={'רעידות מתחזקות'} />
-          <AlertComponent status={'succcess'} title={'אזהרה'} context={''} />
+          <AlertComponent status={'succcess'} title={'אזהרה'} context={''} /> */}
+          {insights.map(ins => <AlertComponent status={statuses[(ins.level_risk)+1]} title={ins.fault_name} description={ins.fault_description} context={ins.insight} treatments={ins.treatments} />)}
         </div>
       </div>
       <div className={styles.charts}>
         <h3>גרפים</h3>
+        <div className={styles.box_button}>
+          <BoxSensorType setSelected={setGraphsBoxType} types={graphsTypes} selected={graphsBoxType} />
+        </div>
       </div>
       <div className={styles.live_info}>
         <h3>מידע בזמן אמת</h3>
