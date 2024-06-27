844ac6696f5adba249bfbf389e7d280133368c11
AviadDerli <aviad@workin.co.il>
2024-06-27 14:13:22 +0300

    fix css and other

--- src/pages/AllGenerators/index.jsx
+++ src/pages/AllGenerators/index.jsx
@@ -11,37 +11,18 @@ import BoxSensorType from '../../components/BoxSensorType';
 // creator: Shahar
 
 function AllGenerators() {
-  // fake data
-  // let data = [{
-  //   _id: '6678464e815884d6e23a4542',
-  //   name: 'gen300',
-  //   location: 'באר שבע',
-  //   status: 'פעיל'
-  // },
-  // {
-  //   _id: '667a89dde30b38c5dad90560',
-  //   name: 'gen102',
-  //   location: 'תל אביב',
-  //   status: 'פעיל'
-  // },
-  // {
-  //   _id: '667a8c00e30b38c5dad90562',
-  //   name: 'gen103',
-  //   location: 'ירושלים',
-  //   status: 'בתיקון'
-  // }]
 
   const getColor = (status) => {
     let color = ''
     status === 'available' ? color = 'green' :
-    status === 'repair' ? color = 'orange' : color = 'red'
+      status === 'repair' ? color = 'orange' : color = 'red'
     return color
   }
-  
+
   const [checked, setChecked] = useState([]);
   const [statusBoxType, setStatusBoxType] = useState('')
   const statuses = [{ text: "הכל", value: '' }, { text: "פעיל", value: 'available' }, { text: "בתיקון", value: 'repair' }, { text: "מושבת", value: 'off' }];
-  
+
   const { data, loading, error } = useApi(`/generator/all-gen?status=${statusBoxType}`)
 
   const handleChange = (id) => {
@@ -65,10 +46,16 @@ function AllGenerators() {
         {data?.map(gen => <div key={gen._id} className={styles.gen}>
           <input type="checkbox" checked={checked.includes(gen._id)} onChange={() => handleChange(gen._id)} />
           <Link to={`/generator/${gen.name}`} className={styles.link}>
-            <h3>{gen.name}</h3>
+            <h1>{gen.name}</h1>
             <h4>{gen.location}</h4>
             <h5 className={styles.status} style={{ color: getColor(gen.status) }}>{gen.status}</h5>
-            {gen.status == 'available' ? <h5>temperature avg: {gen.tempAvg}</h5> : ''}
+            {gen.status == 'available' &&
+              <>
+                <h5>{gen.message ?? <u>'Avg'</u>}</h5>
+                {gen.tempAvg && <h5>temperature: {gen.tempAvg}</h5>}
+                {gen.vibAvg && <h5>vibrtion: {gen.vibAvg}</h5>}
+                {gen.soundAvg && <h5>sound: {gen.soundAvg}</h5>}
+              </>}
           </Link>
         </div>)}
       </div>
