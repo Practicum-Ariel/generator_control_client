959ae96083ec401035e742c0ffcf22af32d4fb2b
Eti Gal-Or <etim139@gmail.com>
2024-06-27 14:55:27 +0300

    generic table  component - deals with nested object

--- src/pages/GenericTable/index.jsx
+++ src/pages/GenericTable/index.jsx
@@ -25,15 +25,15 @@ import { FaRegEye, FaRegTrashAlt, FaRegEdit } from 'react-icons/fa'
 // example below: 
 
 export default function MotherComponent() { // the component that call GenericTable
-  const { data = [], loading, error } = useApi('/technician')
+  const { data = [], loading, error } = useApi('/visit')
   const [columns, setColumns] = useState([])
   const [rows, setRows] = useState([])
   const [tableName, setTableName] = useState('')
 
   useEffect(() => {
-    setTableName('טכנאים')
+    setTableName('היסטוריית בדיקות')
 
-    const columns = [{ header: 'מזהה', accessor: 'index' }, { header: 'שם', accessor: 'fullName' }, { header: 'טלפון', accessor: 'phoneNumber' },]
+    const columns = [{ header: 'מזהה', accessor: 'index' }, { header: 'שם טכנאי', accessor: 'techId.fullName' }, { header: 'נייד', accessor: 'techId.phoneNumber' }, { header: 'שם בדיקה', accessor: 'checklist.title' }, { header: 'תוצאת בדיקה', accessor: 'checklist.value' }]
 
     setColumns(columns)
 
@@ -64,7 +64,18 @@ export default function MotherComponent() { // the component that call GenericTa
 }
 
 // creator: Eti
+// props: { title: string }
 function GenericTable({ tableName, columns, rows, onEdit, onDetails, onDelete }) {
+  const handleTd = (row, col) => {
+    if (col.accessor.includes('.')) {
+      const first = col.accessor.split('.')[0]
+      if (!row[first]) return ''
+      const second = col.accessor.split('.')[1]
+      return row[first][second]
+    }
+    return row[col.accessor]
+  }
+  
   return (
     <section className={styles.genericTable}>
       <h1>{tableName}</h1>
@@ -82,7 +93,7 @@ function GenericTable({ tableName, columns, rows, onEdit, onDetails, onDelete })
             <tr key={row._id}>
               {columns?.map((col) =>
                 <td key={col.accessor}>
-                  {row[col.accessor]}
+                  {handleTd(row, col)}
                 </td>
               )}
               <td>
